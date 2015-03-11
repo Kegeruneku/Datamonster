0447410fa9ee69a8d1b581a9c84089634e326cef
Jason Fagnere <jasonx86.sh@gmail.com>
2015-03-11 17:06:03 +0100

    DTC no2

--- src/link/neolink/datamonster/Datamonster.java
+++ src/link/neolink/datamonster/Datamonster.java
@@ -2,7 +2,7 @@ package link.neolink.datamonster;
 
 /**
  * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
+ * or more contributor license agreements.  See the LICENSE file
  * distributed with this work for additional information
  * regarding copyright ownership.  The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
